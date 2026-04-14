7606cc79b1dace0c0a553b0f54fff482deb9aad5
Danilson Sanches <dsanches06@outlook.com>
2026-04-14 22:26:37 +0100

    Move server code to root /api folder for Vercel

--- api/index.js
+++ api/index.js
@@ -1,4 +1,2 @@
-// Proxy to the actual server in client/api
-const app = require('../client/api/src/server.js');
-
-module.exports = app;
+// Serverless function entry point for Vercel
+module.exports = require('./src/server.js');
