033c8b331d1f60fd2d6f27ef59758bf99fa3a0d2
Danilson Sanches <dsanches06@outlook.com>
2026-04-14 22:02:45 +0100

    Fix image import and vercel config

--- client/src/pages/MainLayout.jsx
+++ client/src/pages/MainLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet, Link } from "react-router";
 import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
+import expenseLogo from "../assets/expense.png";
 import "../styles/MainLayout.css";
 
 const MainLayout = () => {
@@ -14,7 +15,7 @@ const MainLayout = () => {
             <Link to="/" className="logo-link">
               <img
                 className="nav-logo"
-                src="./src/assets/expense.png"
+                src={expenseLogo}
                 alt="logotipo do app"
               />
             </Link>
